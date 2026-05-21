c94e02a18d7554d72eca553cc31067686aed097d
Rishab Raj <rr0948555@gmail.com>
2026-05-21 19:44:21 +0530

    Symbol

---
+++ dist/symbol.js
@@ -0,0 +1,5 @@
+"use strict";
+// Symbol takes unique values
+let id1 = Symbol();
+let id2 = Symbol();
+console.log(id1 === id2);
---
+++ src/symbol.ts
@@ -0,0 +1,6 @@
+// Symbol takes unique values
+
+let id1 = Symbol();
+let id2 = Symbol();
+
+console.log(id1 === id2);
