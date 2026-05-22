5feb87927bab0ac49822f465f2196e308e18ab19
Rishab Raj <rr0948555@gmail.com>
2026-05-22 20:04:29 +0530

    Inference

---
+++ dist/inference.d.ts
@@ -0,0 +1,2 @@
+declare function add2(a: number, b: number): number;
+declare function greet3(name: string): string;
---
+++ dist/inference.js
@@ -0,0 +1,17 @@
+"use strict";
+// Inference automatically detects the datatype
+// let username4 = 'Alex';
+// console.log(typeof username4);
+// let course2 = "TS";
+// course2 = "JS";
+// // course2 = 100; => Not allowed because of string
+// console.log(course2);
+function add2(a, b) {
+    return a + b;
+}
+console.log(add2(10, 5));
+// console.log(typeof  add2(10,5));
+function greet3(name) {
+    return name;
+}
+console.log(greet3("Alex"));
